039e6e08f03b3aab4ffeab126691208309780e54
wangshiyu13 <wangshiyu13z@gmail.com>
2015-01-09 21:46:43 +0800

    Initial Commit

---
+++ News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  News
+//
+//  Created by wangshiyu13 on 15/1/9.
+//  Copyright (c) 2015年 wangshiyu13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ News/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  News
+//
+//  Created by wangshiyu13 on 15/1/9.
+//  Copyright (c) 2015年 wangshiyu13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
